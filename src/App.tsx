59e99e956ab05fb067d2eefb024ae8a7fa4bb000
CJ <cjfritz9@gmail.com>
2023-08-28 10:20:41 -0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Heading } from '@chakra-ui/react';
+
+const App = () => {
+  return (
+    <Box>
+      <Heading>Let's get started!</Heading>
+    </Box>
+  );
+};
+
+export default App;
